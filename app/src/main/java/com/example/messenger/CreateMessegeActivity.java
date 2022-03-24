c232764504b6db3d5799f1914fbb463e3a0221d6
LamerN <90185479+LamerN@users.noreply.github.com>
2022-03-24 16:18:40 +0300

    Create messenger

---
+++ app/src/main/java/com/example/messenger/CreateMessegeActivity.java
@@ -0,0 +1,19 @@
+package com.example.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessegeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_massage);
+    }
+
+    public void onClickSendMessege(View view) {
+
+    }
+}
